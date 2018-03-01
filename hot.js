c4ad9473fe9afebeab6833f14127a0ee246b9f51
Max Metral <max@pyralis.com>
2018-02-28 23:09:26 -0500

    Lint fix

--- hot.js
+++ hot.js
@@ -3,6 +3,7 @@
  * one-off with @gasbuddy/web-dev/hot
  */
 if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line global-require
   const { hot } = require('react-hot-loader');
   module.exports.hot = hot;
 }
