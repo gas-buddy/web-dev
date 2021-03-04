d6f90cc0e6672a351a907288161151655c4d4d4b
Jeffrey Smith <jsmith@gasbuddy.com>
2021-03-04 11:24:39 -0500

    fix: Add support for stage 1 css

--- src/webpack/config.js
+++ src/webpack/config.js
@@ -87,6 +87,9 @@ export function webpackConfig(optionsOrNull) {
                 // If you don't set this, you get the GB preset default,
                 // which is fine in most cases
                 browsers: process.env.BROWSER_SUPPORT,
+                // Setting to stage 1 for now so we don't break functionality
+                // that worked with postcss-cssnext
+                stage: 1,
               }),
             ],
           },
